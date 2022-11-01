224b8e7a020e7ee34e4130df0ef731a81fe3565c
Pranay Pachangane <pranaypachangane@gmail.com>
2022-11-01 12:41:40 +0530

    Updated the project

--- book_bus/app_version_1/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from app_version_1.models import User, Bus, Seat, Transaction
-
-admin.site.register(Transaction)
-admin.site.register(User)
-admin.site.register(Bus)
-admin.site.register(Seat)
-
--- book_bus/app_version_1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppVersion1Config(AppConfig):
-    name = 'app_version_1'
